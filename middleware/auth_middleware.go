e1ffb942a28871128ebd4746336eddf3dd79b673
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2025-02-08 21:40:24 +0700

    apply jwt token middleware into path ev station

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,45 @@
+package middleware
+
+import (
+	"Ev-Charge-Hub/Server/utils"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+// AuthMiddleware validates the JWT token
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
+			c.Abort()
+			return
+		}
+
+		// Expecting "Bearer <token>"
+		tokenParts := strings.Split(authHeader, " ")
+		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization header format"})
+			c.Abort()
+			return
+		}
+
+		// Validate the token
+		token := tokenParts[1]
+		claims, err := utils.ValidateToken(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", claims.UserID)
+		c.Set("userName", claims.Username)
+		c.Set("role", claims.Role)
+
+		// Continue to the next handler
+		c.Next()
+	}
+}
