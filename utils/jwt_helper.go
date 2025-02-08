c8afeee76baadde58feb78b4cfdb5317b0b790ba
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2025-02-08 21:40:24 +0700

    apply jwt token middleware into path ev station

--- utils/jwt_helper.go
+++ utils/jwt_helper.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"errors"
 	"time"
+
 	"github.com/golang-jwt/jwt/v4"
 )
 
@@ -10,13 +12,15 @@ var jwtSecret = []byte("your_jwt_secret")
 type Claims struct {
 	UserID   string `json:"user_id"`
 	Username string `json:"username"`
+	Role     string `json:"role"`
 	jwt.RegisteredClaims
 }
 
-func CreateToken(userID, username string) (string, error) {
+func CreateToken(userID, username string, role string) (string, error) {
 	claims := Claims{
 		UserID:   userID,
 		Username: username,
+		Role:     role,
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
 		},
@@ -24,4 +28,17 @@ func CreateToken(userID, username string) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(jwtSecret)
+}
+
+func ValidateToken(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
 }
